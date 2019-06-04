e0eb882b95dbd10cb4a298d4d590a8331a105e0a
Bob Strahan <strahanr@amazon.com>
2019-06-04 17:23:53 +0000

    fix #2 - CF template fails on bootstrap

--- infrastructure/emr-bootstrap/emr_install_es_proxy.sh
+++ infrastructure/emr-bootstrap/emr_install_es_proxy.sh
@@ -124,44 +124,13 @@ function install_es_proxy() {
   npm config set strict-ssl false
 
   # install aws-es-kibana
-  [ -f /usr/bin/aws-es-kibana ] || npm install "aws-es-kibana@1.0.5" -g --verbose || {
+  [ -f /usr/bin/aws-es-kibana ] || npm install "aws-es-kibana" -g --verbose || {
     echo "$0 - [ERROR] failed to npm install aws-es-kibana" >&2
     exit 1
   }
   # validate that the aws-es-kibana script was installed
   [ -f /usr/bin/aws-es-kibana ] || {
     echo "$0 - [ERROR] aws-es-kibana executable file not found" >&2
-    exit 1
-  }
-
-  # install patch - not present in BeanStalk
-  [ -f /usr/bin/patch ] || yum -y install patch || {
-    echo "$0 - [ERROR] failed to install patch" >&2
-    exit 1
-  }
-  # validate that the patch executable is installed
-  [ -f /usr/bin/patch ] || {
-    echo "$0 - [ERROR] patch executable file not found" >&2
-    exit 1
-  }
-
-  # XXX patching in place as a temporary workaround for EMR large payloads
-  # we should create a more permanent solution with an option
-  patch -f `readlink -f /usr/bin/aws-es-kibana` <<'EOF'
---- a/index.js
-+++ b/index.js
-@@ -109,7 +109,7 @@ app.use(compress());
- if (argv.u && argv.a) {
-   app.use(basicAuth(argv.u, argv.a));
- }
--app.use(bodyParser.raw({type: function() { return true; }}));
-+app.use(bodyParser.raw({limit: '2mb', type: function() { return true; }}));
- app.use(getCredentials);
- app.use(function (req, res) {
-     var bufferStream;
-EOF
-  [ $? ] || {
-    echo "$0 - [ERROR] failed to patch limit in aws-es-kibana" >&2
     exit 1
   }
 
@@ -181,6 +150,11 @@ EOF
     echo "$0 - [ERROR] could not delete running aws-es-kibana proxy" >&2
     exit 1
   })
+
+  # create dummy credentials file (watched by proxy, although we're using cluster role credentials)
+  [ -f ~ec2-user/.aws/credentials ] || {
+    su -c "mkdir -p ~ec2-user/.aws && touch ~ec2-user/.aws/credentials" - ec2-user
+  }
 
   # start proxy
   su -c "pm2 start /usr/bin/aws-es-kibana -- -s -b 127.0.0.1 $endpoint" - ec2-user || {
